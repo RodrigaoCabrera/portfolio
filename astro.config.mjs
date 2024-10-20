04b507bd95e07881312d19fc86c83c73da08c38a
Rodrigo Cabrera <rodrigo.cabrera@infracommerce.lat>
2024-10-20 19:47:41 -0300

    chore: remove unused integrations and set output to "server" in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-import vercel from "@astrojs/vercel/serverless";
-
-import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-
-  integrations: [preact()],
+  output: "server",
 });
